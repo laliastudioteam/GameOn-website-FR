075964131ae6e1ba0d2cc769b761ccd6f564e0c4
LaliaStudio <laliastudio@gmail.com>
2023-12-19 06:41:46 +0100

    Presentation

--- modal.js
+++ modal.js
@@ -7,18 +7,29 @@ function editNav() {
 	}
 }
 
+console.log("start code");
+
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelector(".modal-btn");
 const closeBtn = document.querySelector(".close-btn");
+const closeBtnSuccess = document.querySelector(".close-btn-success");
 const formData = document.querySelector(".formData");
 const formDomSubmit = document.querySelector(".btn-submit");
+
+const formZone = document.getElementById("theForm");
+const validationMessage = document.getElementById("validationMessage");
+const validationMessageZone = document.getElementById("validationMessageZone");
+console.log("Message zone initialisé");
 
 // launch modal event
 modalBtn.addEventListener("click", launchModal);
 
 // close modal event
 closeBtn.addEventListener("click", closeModal);
+
+// close modal event
+closeBtnSuccess.addEventListener("click", closeSuccess);
 
 // sumbit form
 formDomSubmit.addEventListener("click", validate);
@@ -33,9 +44,22 @@ function closeModal() {
 	modalbg.style.display = "none";
 }
 
+// Messages
+const ValidationMessage = "Merci ! Votre réservation a été reçue.";
+
+// close modal form
+function closeSuccess() {
+	modalbg.style.display = "none";
+	theForm.style.display = "block";
+	validationMessage.style.display = "none";
+	validationMessage.classList.add("success_visible");
+}
+
+console.log("test de validation chargé :" + ValidationMessage);
+validationMessageZone.textContent = ValidationMessage;
+
 // checks before submit
 function validate(event) {
-
 	// Prevent Form to validate
 	event.preventDefault();
 
@@ -82,7 +106,7 @@ function validate(event) {
 	function setErrorInput(inputToSet, field) {
 		inputToSet.setAttribute("data-error-visible", "true");
 		errorForm++;
-		console.log( field + " field error data text set to visible");
+		console.log(field + " field error data text set to visible");
 	}
 
 	// Check prenom
@@ -142,8 +166,9 @@ function validate(event) {
 
 	// Check if error to submit .. or not
 	if (errorForm == 0) {
-		let ValidationMessage = "Merci ! Votre réservation a été reçue.";
-		document.getElementById("validation-message").textContent = ValidationMessage;
+		theForm.style.display = "none";
+		validationMessage.style.display = "block";
+		validationMessage.classList.add("success_visible");
 		//document.getElementById('theForm').submit();
 	} else {
 		return false;
