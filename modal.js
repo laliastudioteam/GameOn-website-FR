68d3dc8c53498f012df25b12402f15567fcfc824
LaliaStudio <laliastudio@gmail.com>
2023-12-28 18:03:13 +0100

    Réencodage

--- modal.js
+++ modal.js
@@ -1,5 +1,3 @@
-
-
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
@@ -20,33 +18,41 @@ const checkById = [];
 // Empty error content
 const errorAlerts = {
 	first: [
-		"Le champs prénom ne contient pas au moins " + minFirst + " caractères",
+		"Le prénom contient des chiffres ou compte moins de " + minFirst + " lettres",
+		checkFirst,
 	],
-	last: ["Le champs nom ne contient pas au moins " + minLast + " caractères"],
-	email: ["Le champs email est de forme invalide"],
-	birthdate: ["La date de naissance est invalide"],
-	quantity: [
-		"Le champs quantité doit être un nombre positif",
-		"Le champs quantité ne peut être négatif"
+	last: [
+		"Le nom contient des chiffres ou compte moins de " + minLast + " lettres",
+		checkLast,
 	],
-	location1: ["Le champs location doit être coché"],
-	checkbox1: ["Vous devez accepter les conditions générales"],
+	email: ["L'email est de forme invalide", checkEmail],
+	birthdate: ["La date de naissance est invalide", checkBirthdate],
+	quantity: ["Le champs quantité doit être un nombre positif", checkQuantity],
+	location1: ["Le champs location doit être coché", checkLocation1],
+	checkbox1: ["Vous devez accepter les conditions générales", checkCheckbox1],
 };
 
 let errorDataInput;
 
 // Boucle
 for (let errorMessageKey in errorAlerts) {
-
 	// Get Elements By ID;
 	checkById[errorMessageKey] = document.getElementById(errorMessageKey);
 	errorDataInput = document.getElementById(errorMessageKey).closest(".formData");
 	errorDataInput.setAttribute("data-error", errorAlerts[errorMessageKey][0]);
+
+	if (errorAlerts[errorMessageKey][1]) {
+		// Input Listener
+		checkById[errorMessageKey].addEventListener(
+			"change",
+			errorAlerts[errorMessageKey][1]
+		);
+		console.log("Ecouteur " + errorMessageKey + " acitf");
+	}
 }
 
 // launch modal event
-modalBtn.forEach(item =>item.addEventListener("click", launchModal));
-
+modalBtn.forEach(item => item.addEventListener("click", launchModal));
 
 // close modal event
 closeBtn.addEventListener("click", closeModal);
@@ -62,7 +68,6 @@ closeBtnSuccess.addEventListener("click", closeSuccess);
 
 // close success
 navBtn.addEventListener("click", editNav);
-
 
 // edit nav
 function editNav() {
@@ -106,66 +111,94 @@ function closeSuccess() {
 
 validationMessageZone.textContent = ValidationMessageText;
 
-// checks before submit
-function validate(event) {
+// Check variables
+let inputToTest;
 
-	// Prevent Form to validate
-	event.preventDefault();
+// Empty error count
+let errorForm = 0;
 
-	// Empty error count
-	let errorForm = 0;
+// Function with value
+function setErrorInput(inputToSet, field) {
+	inputToSet.setAttribute("data-error-visible", "true");
+	errorForm++;
+}
 
-	let errorDataInput;
+// Function with value
+function hideErrorInput(inputToSet, field) {
+	inputToSet.setAttribute("data-error-visible", "false");
+}
 
-	// Boucle error messages
-	for (let errorMessageKey in errorAlerts) {
+// Check if only letters
+const expLetters = new RegExp(/\d+/g);
 
-		// Get Elements By ID;
-		checkById[errorMessageKey] = document.getElementById(errorMessageKey);
-		errorDataInput = document.getElementById(errorMessageKey).closest(".formData");
-		errorDataInput.setAttribute("data-error-visible", "false");
-	}
+function onlyLetters(stringTotest) {
+	//	console.log("test: " + stringTotest + " : " + expLetters.test(stringTotest))
+	let testVarNumber = expLetters.test(stringTotest);
+	return testVarNumber;
+}
 
-	// Check variables
-	let inputToTest;
-
-	// Function with value
-	function setErrorInput(inputToSet, field) {
-		inputToSet.setAttribute("data-error-visible", "true");
-		errorForm++;
-	}
-
-	// Check prenom
-	if (checkById["first"].value.length < minFirst) {
+// Firstname validation
+function checkFirst() {
+	hideErrorInput(checkById["first"].closest(".formData"), "first");
+	if (
+		checkById["first"].value.length < minFirst ||
+		onlyLetters(checkById["first"].value)
+	) {
+		//console.log("test2");
 		setErrorInput(checkById["first"].closest(".formData"), "first");
 	}
+}
 
-	// Check nom
-	if (checkById["last"].value.length < minLast) {
+// Lastname validation
+function checkLast() {
+	hideErrorInput(checkById["last"].closest(".formData"), "last");
+	if (
+		checkById["last"].value.length < minFirst ||
+		onlyLetters(checkById["last"].value)
+	) {
+		//console.log("test2");
 		setErrorInput(checkById["last"].closest(".formData"), "last");
 	}
+}
 
-	// Check mail
-	const regularExpressionEmailCheck =
-		/^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
+// Email validation
+const regularExpressionEmailCheck =
+	/^(([^<>()[]\.,;:s@]+(.[^<>()[]\.,;:s@]+)*)|(.+))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,}))$/;
 
+function checkEmail() {
+	hideErrorInput(checkById["email"].closest(".formData"), "email");
 	if (!regularExpressionEmailCheck.test(checkById["email"].value)) {
 		setErrorInput(checkById["email"].closest(".formData"), "email");
 	}
+}
 
-	// Check Birthdate
+// Check Birthdate
+function checkBirthdate() {
+	hideErrorInput(checkById["birthdate"].closest(".formData"), "birthdate");
+
 	let dateObj = new Date(checkById["birthdate"].value);
+	let dateObjJour = Date.now();
 
-	if (checkById["birthdate"].value == "" || isNaN(dateObj)) {
+	if (
+		checkById["birthdate"].value == "" ||
+		isNaN(dateObj) ||
+		dateObj > dateObjJour
+	) {
 		setErrorInput(checkById["birthdate"].closest(".formData"), "birthdate");
 	}
+}
 
-	// Check quantity
+// Check quantity
+function checkQuantity() {
+	hideErrorInput(checkById["quantity"].closest(".formData"), "quantity");
 	if (checkById["quantity"].value == "" || isNaN(checkById["quantity"].value)) {
 		setErrorInput(checkById["quantity"].closest(".formData"), "quantity");
 	}
+}
 
-	// Check location
+// Check location
+function checkLocation1() {
+	hideErrorInput(checkById["location1"].closest(".formData"), "location");
 	let checkLocation = document.getElementsByName("location");
 	let radiochecked = 0;
 
@@ -177,11 +210,53 @@ function validate(event) {
 	if (radiochecked < 1) {
 		setErrorInput(checkById["location1"].closest(".formData"), "location");
 	}
+}
 
-	// Check general conditions
+// Check checkbox1
+function checkCheckbox1() {
+	hideErrorInput(checkById["checkbox1"].closest(".formData"), "checkbox");
 	if (checkById["checkbox1"].checked == false) {
 		setErrorInput(checkById["checkbox1"].closest(".formData"), "checkbox");
 	}
+}
+
+// checks before submit
+function validate(event) {
+	// Prevent Form to validate
+	event.preventDefault();
+
+	errorForm = 0;
+
+	let errorDataInput;
+
+	// Boucle error messages
+	for (let errorMessageKey in errorAlerts) {
+		// Get Elements By ID;
+		checkById[errorMessageKey] = document.getElementById(errorMessageKey);
+		errorDataInput = document.getElementById(errorMessageKey).closest(".formData");
+		errorDataInput.setAttribute("data-error-visible", "false");
+	}
+
+	// Check firstName
+	checkFirst();
+
+	// Check lastName
+	checkLast();
+
+	// Check mail
+	checkEmail();
+
+	// Check birthDate
+	checkBirthdate();
+
+	// Check quantity
+	checkQuantity();
+
+	// Check Location1
+	checkLocation1();
+
+	// Check general conditions
+	checkCheckbox1();
 
 	// Check if error to submit .. or not
 	if (errorForm == 0) {
